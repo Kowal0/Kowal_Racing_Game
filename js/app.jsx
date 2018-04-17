fcb41473f617e92821642c74ff3c46094abe5ca6
Krzysztof Kowalski <kowalski.cv@gmail.com>
2018-04-17 19:00:15 +0200

    added car animations

--- js/app.jsx
+++ js/app.jsx
@@ -91,6 +91,8 @@ document.addEventListener("DOMContentLoaded", function(){
       this.state={
         game_over: false,
 
+        clientHeight: 0,
+
         move_right: false,
         move_left: false,
         move_up: false,
@@ -98,62 +100,127 @@ document.addEventListener("DOMContentLoaded", function(){
 
         player_left: "44%",
         player_bottom: "2%",
+
+        stripe_on: true,
+
+        stripe1: "-150px",
+        stripe2: "150px",
+        stripe3: "450px"
       }
     }
-    //czy mozna dodac event keydown do document
-    //czy zeby manipulowac pozycja diva na keydown potrzebuje referencji
-    turnLeft = (e) => {
+
+    animateBackground = () => {
+      if (this.state.game_over === false) {
+        this.setState({
+          stripe_on: requestAnimationFrame(this.animateBackground),
+          stripe1: parseInt(this.state.stripe1) + 3 + "px",
+          stripe2: parseInt(this.state.stripe2) + 3 + "px",
+          stripe3: parseInt(this.state.stripe3) + 3 + "px",
+        })
+      }
+    }
+
+
+    //moving player
+    turn = (e) => {
       if (this.state.game_over === false) {
         let key = e.keyCode;
         if (key === 37 && this.state.move_left === false) {
-          console.log(key);
-          console.log(e);
-
-          this.setState({
-            move_left: requestAnimationFrame(this.left)
-          })
+            this.setState({
+              move_left: requestAnimationFrame(this.left)
+            })
+        } else if (key === 38 && this.state.move_up === false) {
+            this.setState({
+              move_up: requestAnimationFrame(this.up)
+            })
+        } else if (key === 39 && this.state.move_right === false) {
+            this.setState({
+              move_right: requestAnimationFrame(this.right)
+            })
+        } else if (key === 40 && this.state.move_down === false) {
+            this.setState({
+              move_down: requestAnimationFrame(this.down)
+            })
+          }
         }
       }
-    }
-    turnLeftStop = (e) => {
-      if (this.state.game_over === false) {
+      turnStop = (e) => {
+        if (this.state.game_over === false) {
         let key = e.keyCode;
         if (key === 37) {
           this.setState({
             move_left: cancelAnimationFrame(this.state.move_left),
             move_left: false
           })
-        }
+        } else if (key === 38) {
+          this.setState({
+            move_up: cancelAnimationFrame(this.state.move_up),
+            move_up: false
+          })
+        } else if (key === 39) {
+          this.setState({
+            move_right: cancelAnimationFrame(this.state.move_right),
+            move_right: false
+          })
+        } else if (key === 40) {
+          this.setState({
+            move_down: cancelAnimationFrame(this.state.move_down),
+            move_down: false
+          })
       }
     }
+  }
+
     left = (e) => {
       if (this.state.game_over === false) {
-        console.log(e);
         this.setState({
           player_left: parseInt(this.state.player_left) - 1 + "%",
           move_left: requestAnimationFrame(this.left)
         })
-        console.log(this.state.player_left);
+      }
+    }
+    right = (e) => {
+      if (this.state.game_over === false) {
+        this.setState({
+          player_left: parseInt(this.state.player_left) + 1 + "%",
+          move_right: requestAnimationFrame(this.right)
+        })
+      }
+    }
+    up = (e) => {
+      if (this.state.game_over === false) {
+        this.setState({
+          player_bottom: parseInt(this.state.player_bottom) + 1 + "%",
+          move_up: requestAnimationFrame(this.up)
+        })
+      }
+    }
+    down = (e) => {
+      if (this.state.game_over === false) {
+        this.setState({
+          player_bottom: parseInt(this.state.player_bottom) - 1 + "%",
+          move_down: requestAnimationFrame(this.down)
+        })
       }
     }
     componentDidMount(){
-      document.addEventListener('keydown', e => this.turnLeft(e))
-      document.addEventListener('keyup', e => this.turnLeftStop(e))
+      document.addEventListener('keydown', e => this.turn(e))
+      document.addEventListener('keyup', e => this.turnStop(e))
+      document.addEventListener('load', this.animateBackground());
+
+      const height = document.querySelector('.road').clientHeight;
+      this.setState({ clientHeight: this.height });
+      console.log(height); //set state jest asynchroniczny, dlatego nie lapie heigh
     }
      render() {
-       // console.log(this.turnLeft);
-       // document.addEventListener('keydown', function(e) {
-       //   console.log(turn);
-       //   turnLeft(e)
-       // });
        return (
          <div className="road" >
-           <div className="stripe stripe-l stripe1"></div>
-           <div className="stripe stripe-l stripe2"></div>
-           <div className="stripe stripe-l stripe3"></div>
-           <div className="stripe stripe-r stripe1"></div>
-           <div className="stripe stripe-r stripe2"></div>
-           <div className="stripe stripe-r stripe3"></div>
+           <div className="stripe stripe-l" style={{top:this.state.stripe1}}></div>
+           <div className="stripe stripe-l" style={{top:this.state.stripe2}}></div>
+           <div className="stripe stripe-l" style={{top:this.state.stripe3}}></div>
+           <div className="stripe stripe-r" style={{top:this.state.stripe1}}></div>
+           <div className="stripe stripe-r" style={{top:this.state.stripe2}}></div>
+           <div className="stripe stripe-r" style={{top:this.state.stripe3}}></div>
            <div className="car car-player" style={{left:this.state.player_left, bottom:this.state.player_bottom}}></div>
            <div className="car car-1"></div>
            <div className="car car-2"></div>
